7fa9aa95d76302aef20b816a59ad5da95ba9151a
Jittiga <sri-harsha.jittiga@capgemini.com>
2023-03-14 15:13:19 +0530

    NEW PROJECT TO PUSH TO GITHUB

---
+++ Spring-Email-Client/src/test/java/com/dailycodebuffer/springemailclient/SpringEmailClientApplicationTests.java
@@ -0,0 +1,15 @@
+package com.dailycodebuffer.springemailclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+
+@SpringBootTest(classes = {SpringEmailClientApplicationTests.class})
+class SpringEmailClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
